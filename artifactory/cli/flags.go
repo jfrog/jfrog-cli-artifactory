c2e5f0013b1a32a7205f5e8d885fa6968be454c2
agrasthn <140591594+agrasth@users.noreply.github.com>
2025-03-13 22:33:45 +0530

    Migrating rt flags

--- artifactory/cli/flags.go
+++ artifactory/cli/flags.go
@@ -1,7 +1,6 @@
-package flagkit
+package cli
 
 import (
-	"github.com/jfrog/jfrog-cli-artifactory/cliutils/cmddefs"
 	commonCliUtils "github.com/jfrog/jfrog-cli-core/v2/common/cliutils"
 	pluginsCommon "github.com/jfrog/jfrog-cli-core/v2/plugins/common"
 	"github.com/jfrog/jfrog-cli-core/v2/plugins/components"
@@ -9,17 +8,6 @@ import (
 )
 
 const (
-	DownloadMinSplitKb = 5120
-	DownloadSplitCount = 3
-
-	// Upload
-	UploadMinSplitMb  = 200
-	UploadSplitCount  = 5
-	UploadChunkSizeMb = 20
-
-	// Common
-	Retries                = 3
-	ArtifactoryTokenExpiry = 3600
 
 	// Artifactory's Commands Keys
 	Upload                 = "upload"
@@ -53,6 +41,7 @@ const (
 	BuildDockerCreate      = "build-docker-create"
 	OcStartBuild           = "oc-start-build"
 	NpmConfig              = "npm-config"
+	Npm                    = "npm"
 	NpmInstallCi           = "npm-install-ci"
 	NpmPublish             = "npm-publish"
 	PnpmConfig             = "pnpm-config"
@@ -262,7 +251,8 @@ const (
 	configFlag = "config"
 
 	// Unique build-scan flags
-	fail = "fail"
+	fail   = "fail"
+	rescan = "rescan"
 
 	// Unique build-promote flags
 	buildPromotePrefix  = "bpr-"
@@ -434,15 +424,17 @@ const (
 	configPassword    = configPrefix + password
 	configInsecureTls = configPrefix + InsecureTls
 
-	// Generic commands flags
-	name            = "name"
 	IncludeRepos    = "include-repos"
 	ExcludeRepos    = "exclude-repos"
 	IncludeProjects = "include-projects"
 	ExcludeProjects = "exclude-projects"
 
+	// *** JFrog Pipelines Commands' flags ***
+	// Base flags
+	name = "name"
+	Sync = "sync"
+
 	// Unique lifecycle flags
-	Sync                 = "sync"
 	lifecyclePrefix      = "lc-"
 	lcSync               = lifecyclePrefix + Sync
 	lcProject            = lifecyclePrefix + Project
@@ -463,52 +455,6 @@ const (
 )
 
 var commandFlags = map[string][]string{
-	cmddefs.ReleaseBundleV1Create: {
-		distUrl, user, password, accessToken, serverId, specFlag, specVars, targetProps,
-		rbDryRun, sign, desc, exclusions, releaseNotesPath, releaseNotesSyntax, rbPassphrase, rbRepo, InsecureTls, distTarget, rbDetailedSummary,
-	},
-	cmddefs.ReleaseBundleV1Update: {
-		distUrl, user, password, accessToken, serverId, specFlag, specVars, targetProps,
-		rbDryRun, sign, desc, exclusions, releaseNotesPath, releaseNotesSyntax, rbPassphrase, rbRepo, InsecureTls, distTarget, rbDetailedSummary,
-	},
-	cmddefs.ReleaseBundleV1Sign: {
-		distUrl, user, password, accessToken, serverId, rbPassphrase, rbRepo,
-		InsecureTls, rbDetailedSummary,
-	},
-	cmddefs.ReleaseBundleV1Distribute: {
-		distUrl, user, password, accessToken, serverId, rbDryRun, DistRules,
-		site, city, countryCodes, sync, maxWaitMinutes, InsecureTls, CreateRepo,
-	},
-	cmddefs.ReleaseBundleV1Delete: {
-		distUrl, user, password, accessToken, serverId, rbDryRun, DistRules,
-		site, city, countryCodes, sync, maxWaitMinutes, InsecureTls, deleteFromDist, deleteQuiet,
-	},
-	cmddefs.ReleaseBundleCreate: {
-		platformUrl, user, password, accessToken, serverId, lcSigningKey, lcSync, lcProject, lcBuilds, lcReleaseBundles,
-		specFlag, specVars, BuildName, BuildNumber,
-	},
-	cmddefs.ReleaseBundlePromote: {
-		platformUrl, user, password, accessToken, serverId, lcSigningKey, lcSync, lcProject, lcIncludeRepos,
-		lcExcludeRepos, PromotionType,
-	},
-	cmddefs.ReleaseBundleDistribute: {
-		platformUrl, user, password, accessToken, serverId, lcProject, DistRules, site, city, countryCodes,
-		lcDryRun, CreateRepo, lcPathMappingPattern, lcPathMappingTarget, lcSync, maxWaitMinutes,
-	},
-	cmddefs.ReleaseBundleDeleteLocal: {
-		platformUrl, user, password, accessToken, serverId, deleteQuiet, lcSync, lcProject,
-	},
-	cmddefs.ReleaseBundleDeleteRemote: {
-		platformUrl, user, password, accessToken, serverId, deleteQuiet, lcDryRun, DistRules, site, city, countryCodes,
-		lcSync, maxWaitMinutes, lcProject,
-	},
-	cmddefs.ReleaseBundleExport: {
-		platformUrl, user, password, accessToken, serverId, lcPathMappingTarget, lcPathMappingPattern, Project,
-		downloadMinSplit, downloadSplitCount,
-	},
-	cmddefs.ReleaseBundleImport: {
-		user, password, accessToken, serverId, platformUrl,
-	},
 	AddConfig: {
 		interactive, EncPassword, configPlatformUrl, configRtUrl, configDistUrl, configXrUrl, configMcUrl, configPlUrl, configUser, configPassword, configAccessToken, sshKeyPath, sshPassphrase, ClientCertPath,
 		ClientCertKeyPath, BasicAuthOnly, configInsecureTls, Overwrite, passwordStdin, accessTokenStdin,
@@ -1009,40 +955,6 @@ var flagsMap = map[string]components.Flag{
 
 	//GroupCreate
 	Replace: components.NewBoolFlag(Replace, "[Default: false] Set to true if you'd like existing groups to be replaced.", components.WithBoolDefaultValueFalse()),
-
-	distUrl:              components.NewStringFlag(url, "JFrog Distribution URL. (example: https://acme.jfrog.io/distribution)", components.SetMandatoryFalse()),
-	targetProps:          components.NewStringFlag(targetProps, "List of semicolon-separated(;) properties, in the form of \"key1=value1;key2=value2;...\" to be added to the artifacts after distribution of the release bundle.", components.SetMandatoryFalse()),
-	rbDryRun:             components.NewBoolFlag(dryRun, "Set to true to disable communication with JFrog Distribution.", components.WithBoolDefaultValueFalse()),
-	sign:                 components.NewBoolFlag(sign, "If set to true, automatically signs the release bundle version.", components.WithBoolDefaultValueFalse()),
-	desc:                 components.NewStringFlag(desc, "Description of the release bundle.", components.SetMandatoryFalse()),
-	releaseNotesPath:     components.NewStringFlag(releaseNotesPath, "Path to a file describes the release notes for the release bundle version.", components.SetMandatoryFalse()),
-	releaseNotesSyntax:   components.NewStringFlag(releaseNotesSyntax, "The syntax for the release notes. Can be one of 'markdown', 'asciidoc', or 'plain_text.", components.SetMandatoryFalse()),
-	rbPassphrase:         components.NewStringFlag(passphrase, "The passphrase for the signing key.", components.SetMandatoryFalse()),
-	rbRepo:               components.NewStringFlag(repo, "A repository name at source Artifactory to store release bundle artifacts in. If not provided, Artifactory will use the default one.", components.SetMandatoryFalse()),
-	distTarget:           components.NewStringFlag(target, "The target path for distributed artifacts on the edge node.", components.SetMandatoryFalse()),
-	rbDetailedSummary:    components.NewBoolFlag(detailedSummary, "Set to true to get a command summary with details about the release bundle artifact.", components.WithBoolDefaultValueFalse()),
-	DistRules:            components.NewStringFlag(DistRules, "Path to distribution rules.", components.SetMandatoryFalse()),
-	site:                 components.NewStringFlag(site, "Wildcard filter for site name.", components.SetMandatoryFalse()),
-	city:                 components.NewStringFlag(city, "Wildcard filter for site city name.", components.SetMandatoryFalse()),
-	countryCodes:         components.NewStringFlag(countryCodes, "List of semicolon-separated(;) wildcard filters for site country codes.", components.SetMandatoryFalse()),
-	sync:                 components.NewBoolFlag(sync, "Set to true to enable sync distribution (the command execution will end when the distribution process ends).", components.WithBoolDefaultValueFalse()),
-	maxWaitMinutes:       components.NewStringFlag(maxWaitMinutes, "Max minutes to wait for sync distribution.", components.WithStrDefaultValue("60")),
-	deleteFromDist:       components.NewBoolFlag(deleteFromDist, "Set to true to delete release bundle version in JFrog Distribution itself after deletion is complete.", components.WithBoolDefaultValueFalse()),
-	CreateRepo:           components.NewBoolFlag(CreateRepo, "Set to true to create the repository on the edge if it does not exist.", components.WithBoolDefaultValueFalse()),
-	lcSync:               components.NewBoolFlag(Sync, "Set to false to run asynchronously.", components.WithBoolDefaultValueTrue()),
-	lcProject:            components.NewStringFlag(Project, "Project key associated with the Release Bundle version.", components.SetMandatoryFalse()),
-	lcBuilds:             components.NewStringFlag(Builds, "Path to a JSON file containing information of the source builds from which to create a release bundle.", components.SetHiddenStrFlag(), components.SetMandatoryFalse()),
-	lcReleaseBundles:     components.NewStringFlag(ReleaseBundles, "Path to a JSON file containing information of the source release bundles from which to create a release bundle.", components.SetHiddenStrFlag(), components.SetMandatoryFalse()),
-	lcSigningKey:         components.NewStringFlag(SigningKey, "The GPG/RSA key-pair name given in Artifactory. If the key isn't provided, the command creates or uses the default key.", components.SetMandatoryFalse()),
-	lcPathMappingPattern: components.NewStringFlag(PathMappingPattern, "Specify along with "+PathMappingTarget+" to distribute artifacts to a different path on the edge node. You can use wildcards to specify multiple artifacts.", components.SetMandatoryFalse()),
-	lcPathMappingTarget: components.NewStringFlag(PathMappingTarget, "The target path for distributed artifacts on the edge node. If not specified, the artifacts will have the same path and name on the edge node, as on the source Artifactory server. "+
-		"For flexibility in specifying the distribution path, you can include placeholders in the form of {1}, {2} which are replaced by corresponding tokens in the pattern path that are enclosed in parenthesis.` `", components.SetMandatoryFalse()),
-	lcDryRun: components.NewBoolFlag(dryRun, "Set to true to only simulate the distribution of the release bundle.", components.WithBoolDefaultValueFalse()),
-	lcIncludeRepos: components.NewStringFlag(IncludeRepos, "List of semicolon-separated(;) repositories to include in the promotion. If this property is left undefined, all repositories (except those specifically excluded) are included in the promotion. "+
-		"If one or more repositories are specifically included, all other repositories are excluded.` `", components.SetMandatoryFalse()),
-	lcExcludeRepos: components.NewStringFlag(ExcludeRepos, "List of semicolon-separated(;) repositories to exclude from the promotion.` `", components.SetMandatoryFalse()),
-	platformUrl:    components.NewStringFlag(url, "JFrog platform URL. (example: https://acme.jfrog.io)` `", components.SetMandatoryFalse()),
-	PromotionType:  components.NewStringFlag(PromotionType, "The promotion type. Can be one of 'copy' or 'move'.", components.WithStrDefaultValue("copy")),
 }
 
 func GetCommandFlags(cmdKey string) []components.Flag {
