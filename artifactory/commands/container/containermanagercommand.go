c7c68f11fc683106ae064d1245f9bffbace839c2
agrasthn <140591594+agrasth@users.noreply.github.com>
2025-05-12 11:37:21 +0530

    Refactor: Remove redundant validateSha field and methods from ContainerCommand

--- artifactory/commands/container/containermanagercommand.go
+++ artifactory/commands/container/containermanagercommand.go
@@ -11,7 +11,6 @@ type ContainerCommand struct {
 	skipLogin            bool
 	cmdParams            []string
 	containerManagerType container.ContainerManagerType
-	validateSha          bool
 }
 
 func NewContainerManagerCommand(containerManagerType container.ContainerManagerType) *ContainerCommand {
@@ -28,15 +27,6 @@ func (cm *ContainerCommand) SetSkipLogin(skipLogin bool) *ContainerCommand {
 func (cm *ContainerCommand) SetCmdParams(cmdParams []string) *ContainerCommand {
 	cm.cmdParams = cmdParams
 	return cm
-}
-
-func (cm *ContainerCommand) SetValidateSha(validateSha bool) *ContainerCommand {
-	cm.validateSha = validateSha
-	return cm
-}
-
-func (cm *ContainerCommand) IsValidateSha() bool {
-	return cm.validateSha
 }
 
 func (cm *ContainerCommand) PerformLogin(serverDetails *config.ServerDetails, containerManagerType container.ContainerManagerType) error {
--- go.mod
+++ go.mod
@@ -132,7 +132,7 @@ require (
 	sigs.k8s.io/yaml v1.4.0 // indirect
 )
 
-//replace github.com/jfrog/jfrog-cli-core/v2 => github.com/jfrog/jfrog-cli-core/v2 v2.31.1-0.20250429081008-4c70b8d467b9
+replace github.com/jfrog/jfrog-cli-core/v2 => github.com/agrasth/jfrog-cli-core/v2 v2.0.0-20250516225526-333c9f5b3686
 
 //replace github.com/jfrog/jfrog-client-go => github.com/jfrog/jfrog-client-go v1.28.1-0.20250508130334-f159cff9b11a
 
