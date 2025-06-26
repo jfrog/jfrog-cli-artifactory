e3021daa04fe2f6d1e43bb402abc07ec4f8135ba
mnsboev <m.n.sboev@gmail.com>
2025-06-26 13:01:09 +0300

    Fix path in verify_custom for graphql query

--- evidence/verify_custom.go
+++ evidence/verify_custom.go
@@ -31,22 +31,15 @@ func NewVerifyEvidencesCustom(serverDetails *coreConfig.ServerDetails, subjectRe
 
 // Run executes the custom evidence verification command.
 func (v *VerifyEvidenceCustom) Run() error {
-	split := strings.Split(v.subjectRepoPath, "/")
-	repo := split[0]
-	name := split[len(split)-1]
-	path := strings.Join(split[1:len(split)-1], "/")
+	repo, path, name, err := extractSubjectRepoPathName(v)
+	if err != nil {
+		return err
+	}
 	client, err := v.createArtifactoryClient()
 	if err != nil {
 		return fmt.Errorf("failed to create Artifactory client: %w", err)
 	}
-	var subjectPath string
-	if path != "" {
-		path = fmt.Sprintf(optionalPathTemplate, path)
-		subjectPath = fmt.Sprintf("%s/%s/%s", repo, path, name)
-	} else {
-		subjectPath = fmt.Sprintf("%s/%s", repo, name)
-	}
-	query := fmt.Sprintf(aqlCustomQueryTemplate, repo, path, name)
+	query := prepareAqlQuery(path, repo, name)
 	result, err := ExecuteAqlQuery(query, client)
 	if err != nil {
 		return fmt.Errorf("failed to execute AQL query: %w", err)
@@ -54,12 +47,23 @@ func (v *VerifyEvidenceCustom) Run() error {
 	if len(result.Results) == 0 {
 		return fmt.Errorf("no subject found for %s/%s/%s", repo, path, name)
 	}
-	subjectSha256 := result.Results[0].Sha256
 	metadata, err := v.queryEvidenceMetadata(repo, path, name)
 	if err != nil {
 		return err
 	}
-	return v.verifyEvidences(client, metadata, subjectSha256, subjectPath)
+	subjectSha256 := result.Results[0].Sha256
+	return v.verifyEvidences(client, metadata, subjectSha256, v.subjectRepoPath)
+}
+
+func extractSubjectRepoPathName(v *VerifyEvidenceCustom) (string, string, string, error) {
+	split := strings.Split(v.subjectRepoPath, "/")
+	if len(split) < 2 {
+		return "", "", "", fmt.Errorf("invalid subject repository path: %s. Expected format: <repo>/<path>/<name>", v.subjectRepoPath)
+	}
+	repo := split[0]
+	name := split[len(split)-1]
+	path := strings.Join(split[1:len(split)-1], "/")
+	return repo, path, name, nil
 }
 
 // ServerDetails returns the server details for the command.
@@ -70,4 +74,12 @@ func (v *VerifyEvidenceCustom) ServerDetails() (*coreConfig.ServerDetails, error
 // CommandName returns the name of the command.
 func (v *VerifyEvidenceCustom) CommandName() string {
 	return "verify-evidence-custom"
+}
+
+func prepareAqlQuery(path string, repo string, name string) string {
+	if path != "" {
+		path = fmt.Sprintf(optionalPathTemplate, path)
+	}
+	query := fmt.Sprintf(aqlCustomQueryTemplate, repo, path, name)
+	return query
 }
